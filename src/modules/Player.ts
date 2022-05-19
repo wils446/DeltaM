3f840384b57e9a820639563774d8e3875ee6a2c3
wils446 <wilsonlimmmm@gmail.com>
2022-05-19 13:05:44 +0700

    add Player and Queue modules

---
+++ src/modules/Player.ts
@@ -0,0 +1,32 @@
+import { Player as DefaultPlayer, PlayerOptions } from "discord-music-player";
+import { Client as DiscordClient, Collection, Snowflake, TextBasedChannel } from "discord.js";
+import Queue from "./Queue";
+
+class Player extends DefaultPlayer {
+	public queues: Collection<Snowflake, Queue> = new Collection();
+
+	constructor(client: DiscordClient, options?: PlayerOptions) {
+		super(client, options);
+	}
+
+	createQueue(guildId: Snowflake, options: PlayerOptions & { channel?: TextBasedChannel } = this.options): Queue {
+		options = Object.assign({} as PlayerOptions, this.options, options);
+
+		const guild = this.client.guilds.resolve(guildId);
+		if (!guild || !options.channel) throw new Error("Guild / Chanenl not found");
+		if (this.hasQueue(guildId)) return this.getQueue(guildId) as Queue;
+
+		const { channel } = options;
+		delete options.channel;
+		const queue = new Queue(channel, this, guild, options);
+		this.setQueue(guildId, queue);
+
+		return queue;
+	}
+
+	getQueue(guildId: Snowflake): Queue | undefined {
+		return this.queues.get(guildId);
+	}
+}
+
+export default Player;
---
+++ src/modules/Queue.ts
@@ -0,0 +1,80 @@
+import { Player, PlayerOptions, Queue as DefaultQueue, Song, Utils } from "discord-music-player";
+import { Client as Youtube, VideoCompact } from "youtubei";
+import { Guild, TextBasedChannel } from "discord.js";
+
+class Queue extends DefaultQueue {
+	autoplay = false;
+	lastPlayedSongs: Song[] = [];
+	youtube: Youtube;
+	channel: TextBasedChannel;
+
+	constructor(channel: TextBasedChannel, player: Player, guild: Guild, options?: PlayerOptions) {
+		super(player, guild, options);
+		this.youtube = new Youtube();
+		this.channel = channel;
+	}
+
+	setAutoPlay(value: boolean): void {
+		this.autoplay = value;
+	}
+
+	get lastPlayed(): Song {
+		return this.lastPlayedSongs[this.lastPlayedSongs.length - 1];
+	}
+
+	private onQueueEnd(): void {
+		if (this.destroyed) return;
+		if (this.lastPlayed && this.autoplay && this.songs.length === 0) this.autoPlay();
+	}
+
+	onSongChanged(song: Song): void {
+		if (this.destroyed || !song) return;
+		if (!this.lastPlayedSongs.find((s) => s.url === song.url)) {
+			this.lastPlayedSongs.push(song);
+			if (this.lastPlayedSongs.length > 5) this.lastPlayedSongs.shift();
+		}
+
+		this.channel.send(`${song.name} has been added to the queue.`);
+	}
+
+	private async autoPlay(): Promise<Song> {
+		if (!this.lastPlayed) throw new Error("No last played song");
+		let lastVideoId: string | null;
+
+		try {
+			lastVideoId = new URL(this.lastPlayed.url).searchParams.get("v");
+			if (!lastVideoId) throw new Error();
+		} catch (err) {
+			throw new Error("Not a Youtube Video");
+		}
+
+		const video = await this.youtube.getVideo(lastVideoId);
+		if (!video) throw new Error("Video not found");
+
+		const lastPlayedIds = this.lastPlayedSongs.map((s) => new URL(s.url).searchParams.get("v"));
+		const relatedVideos = [video.upNext, ...video.related].filter(
+			(r) => r instanceof VideoCompact && !lastPlayedIds.includes(r.id)
+		) as VideoCompact[];
+
+		const relatedVideo = relatedVideos.shift();
+		if (!relatedVideo) throw new Error("no telated video found");
+
+		const song = new Song(
+			{
+				author: relatedVideo.channel?.name || "",
+				duration: Utils.msToTime(relatedVideo.duration || 0),
+				isLive: relatedVideo.isLive,
+				name: relatedVideo.title,
+				thumbnail: relatedVideo.thumbnails.best || "_",
+				url: `https://www.youtube.com/watch?v=${relatedVideo.id}`,
+			},
+			this,
+			this.lastPlayed.requestedBy
+		);
+
+		this.play(song);
+		return song;
+	}
+}
+
+export default Queue;
