6c9e15e455bb344ffdd873cc27d83411c3ddd123
wils446 <wilsonlimmmm@gmail.com>
2022-05-20 18:45:25 +0700

    add types into discord.js

---
+++ src/@types/discord.d.ts
@@ -0,0 +1,30 @@
+import "discord.js";
+import Player from "../modules/Player";
+import Queue from "../modules/Queue";
+
+declare module "discord.js" {
+	export interface Client {
+		commands: Command[];
+		player: Player;
+	}
+
+	export interface Guild {
+		queue?: Queue;
+	}
+
+	export interface Command<ButtonInteractionMeta = unknown> {
+		name: string;
+		description: string;
+		aliases?: string[];
+		args?: CommandArgs[];
+		enabled?: boolean;
+		execute: (message: Message, args: string[]) => Promise<unknown>;
+		buttonInteractionIdPrefix?: string;
+		buttonInteractionIdParser?: (id: string) => ButtonInteractionMeta;
+		buttonInteractionIdArgs?: string[];
+		buttonInteraction?: (
+			interaction: ButtonInteraction,
+			meta: ButtonInteractionMeta extends unknown ? unknown : ButtonInteractionMeta
+		) => Promise<unknown>;
+	}
+}
