28b7951dcc8d11e112fde567cfda187eae8cb6f3
wils446 <wilsonlimmmm@gmail.com>
2022-06-27 00:25:45 +0700

    add aliases

--- src/commands/stop/index.ts
+++ src/commands/stop/index.ts
@@ -3,6 +3,7 @@ import { Command } from "discord.js";
 const command: Command = {
 	name: "stop",
 	description: "Stop playing song",
+	aliases: ["dc", "disconnect", "leave"],
 	async execute(message) {
 		const queue = message.guild?.queue;
 		if (!queue) return;
