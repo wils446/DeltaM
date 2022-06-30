dcfce76fc3cda04d041e3afbf676571ac0d6e24f
wils446 <wilsonlimmmm@gmail.com>
2022-06-30 14:13:52 +0700

    add command stop

---
+++ src/commands/stop.ts
@@ -0,0 +1,23 @@
+import { ICommand } from "discord.js";
+
+const command: ICommand = {
+	name: "stop",
+	description: "Stop playing song",
+	aliases: ["dc", "disconnect", "leave"],
+	async execute(message) {
+		const queue = message.guild?.queue;
+		if (!queue) return;
+
+		if (message.member?.voice.channel?.id !== queue.voiceChannel?.id) {
+			await message.reply("Join dlu baru request :smirk:");
+			return;
+		}
+
+		queue.stop();
+
+		await message.react("🛑");
+		await message.react("👌🏻");
+	},
+};
+
+export default command;
