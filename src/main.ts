433009f82c4ac1eba4d20ceb248988247f224480
wils446 <wilsonlimmmm@gmail.com>
2022-06-30 14:20:03 +0700

    add messageHandler

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Client, Intents } from "discord.js";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config({ path: ".env" });
 
@@ -15,10 +16,35 @@ const client = new Client({
 	],
 });
 
+client.commands = [];
+
+const commandFiles = fs.readdirSync("./src/commands");
+for (const file of commandFiles) {
+	const { default: command } = require(`./commands/${file}`);
+	client.commands.push(command);
+}
+
 client.once("ready", () => {
 	console.log("Ready!");
 });
 
-client.on("messageCreate", async (msg) => {});
+client.on("messageCreate", async (message) => {
+	if (message.author.bot || !message.cleanContent.startsWith(PREFIX)) return;
+
+	const args = message.cleanContent.slice(PREFIX.length).split(/ +/);
+	const commandName = (args.shift() as string).toLowerCase();
+
+	const command = client.commands.find((cmd) => cmd.name === commandName || cmd.aliases?.includes(commandName));
+	if (!command || !message.guild) return;
+
+	try {
+		const queue = client.player.getQueue(message.guild.id);
+		message.guild.queue = queue;
+
+		await command.execute(message, args);
+	} catch (err) {
+		await message.reply(`Failed to executethe command : ${(err as Error).message}`);
+	}
+});
 
 client.login(TOKEN);
